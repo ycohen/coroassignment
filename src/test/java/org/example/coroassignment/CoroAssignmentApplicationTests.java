c92d6a66a61a13980fe0da7d7c581c10d688486c
Yonatan Cohen <yonatanmc@gmail.com>
2025-07-14 21:22:41 +0300

    Initial commit

---
+++ src/test/java/org/example/coroassignment/CoroAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.coroassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoroAssignmentApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
